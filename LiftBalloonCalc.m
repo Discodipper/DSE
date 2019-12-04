ecaeb83659f82b01e1a0e63a52213efc7afe60a5
dschoon98 <adpschoon@gmail.com>
2019-12-04 17:28:12 +0100

    grugshjvh

--- LiftBalloonCalc.m
+++ LiftBalloonCalc.m
@@ -1,14 +1,9 @@
 % ISA CALCULATIONS
 g = 9.80665; %gravity constant m/s^2
-h = [];
+radiation = [0,1.243505181776943, 2.439532955387986, 3.5893544197450122, 4.6942214931527735, 5.755366973179216, 6.774004597073443, 7.751329102742852, 8.688516290301822, 9.586723084204786, 10.447087595976798, 11.270729187555142, 12.058748535256372, 12.812227694383278, 13.532230164486982, 14.219800955300181, 14.875966653357649, 15.501735489321305, 16.098097406027478, 16.767416608916115, 17.407330073079443, 17.998751436019802, 18.54535538816441, 19.050538155007143, 19.517438598945684, 19.948957722039346, 20.347776690864386, 20.716373495461028, 21.057038345879562, 21.37188790198919, 21.66287842496402, 21.93181793216053, 22.180377430908745, 22.41010130101615, 22.62241689049432, 22.818643384129427, 23.000000000000004];
 i = 1;
-F_b = [];
-F_bc = [];
-%Outp_year = [];
-hc = [];
 Num_solp = [];
-% SOLAR PANEL CALCULATIONS
-Num_balloon = 100;
+Num_balloon = 50;
 A_sp = 0.0027; % [m^2]
 t_solp = 0.008; %[m]
 W_solp = 0.135*g; % [N]
@@ -18,18 +13,32 @@ Outp_0 = 1.155; % Output van 1 solar panel [J/s]
 eff_0 = 0.316; %  [frac]
 tot_outp_0 = Outp_0/eff_0; % [W]
 temp_coef = -0.004; % [frac/deg C] 
-Req_eng = 70000; % [MWh] TU Delft yearly energy requirement
+Req_eng = 67908; % [MWh] TU Delft yearly energy requirement
+Vol_balloon = [];
+Area_solp = [];
+Tot_W_solp = [];
+Tot_area_solp = [];
+Vol = [];
+Outp = [];
 
-%Weights
-W_antenna = 5278; %[N]
-W_balloon = 0; %[N]
-W_cables = 300; %[N]
-W_battery = 10000; %[N]
+% ---------------------------- WEIGHTS --------------------------------
+W_antenna = 80000; %[N]
+W_film = 0; %[N]
+W_cables = 0; %[N]
+W_battery = 0; %[N]
 
+% ---------------------------- MICROWAVES ----------------------------------------
+D = 2000; % [m] seperation between antennas
+c = 299792458; % [m/s] speed of microwaves
+f = 2.45 * 10^9; % [1/s] frequency of waves
+labda = c/f; % [m] wavelength
+eff_dc_rf = 0.85; % [-]
+eff_atm = 0.722358; % [-]
+A_r = 1000; % [m^2] 
+A_t = [];
+% ---------------------------- ALTITUDE-----------------------------
 
 for alt = 2000:500:20000
-    %[T, rho, p] = airdensity_calculator(alt);
-    
     if (0<alt)&& (alt<=11000)
         p_0 = 101325; %Pa
         T_0 = 288.15; %K
@@ -39,8 +48,7 @@ for alt = 2000:500:20000
         T = T_0 + a_lapse * (alt-h_0); %Temperature for gradient layer
         p = p_0 * (T/T_0)^(-g/(a_lapse*R_air));
         rho = p/(R_air*T);
-        
-        
+                
     elseif (11000<alt) && (alt<=20000)
         p_0 = 22700; %Pa
         h_0 = 11000;
@@ -54,14 +62,18 @@ for alt = 2000:500:20000
     Temp_diff = T - T_b;
     frac_diff = Temp_diff * temp_coef;
     eff = eff_0 + frac_diff;
-    Outp(i) = tot_outp_0 * eff * 5; % Total output * efficiency * 5 for being above clouds LOL
+    x(i) = ((radiation(i)/100)+1); % Increase of output due to solar radiation 
+    Outp(i) = tot_outp_0 * eff * 5 * x(i); % Total output * efficiency * 5 for being above clouds LOL
+    
     Outp_year(i) = (Outp(i)*3600*24*365)/(3600*1000000); %Output of 1 solar panel per year in [MWh]
     Num_solp(i) = ceil(Req_eng/Outp_year(i)); % Minimum number of solar panels
     Tot_W_solp(i) = Num_solp(i) * W_solp;
+    Tot_W(i) = Tot_W_solp(i) + W_antenna + W_film + W_cables + W_battery;
+    
     
     % Balloon calculations when stationary altitude:
     rho_i = 0.0837765634; % density inside balloon
-    Vol(i) = Tot_W_solp(i) / (g * (rho - rho_i)); % Volume entire system when stationary[m^3]
+    Vol(i) = Tot_W(i) / (g * (rho - rho_i)); % Volume entire system when stationary[m^3]
     Tot_area_solp(i) = (Tot_W_solp(i) / rho_solp) / t_solp; %Total area solp of system [m^2]
     
     %Horizontal spacing calculations
@@ -70,22 +82,23 @@ for alt = 2000:500:20000
     Area_solp(i) = Tot_area_solp(i) / Num_balloon; %area of solp per balloon
     %Spacing(i)  = (Area_solp(i) + 200*200) * Num_balloon; %Spacing needed for entire system 
     
+    % ---------------------------- MICROWAVES ----------------------------------------
+    P_t_tot(i) = Outp(i) *eff_dc_rf; % [W] Power transmitted for entire system
+    P_r_tot(i) = P_t_tot(i) * eff_atm; % [W] Power recieved for entire system
+    A_t(i) = (P_r_tot(i) * labda^2 * alt^2)/(A_r*P_t_tot(i)); 
     
-    
-    %D = C_D * 0.5 * V^2 * Area_balloon * rho
-    %W(i) = (Tot_W_solp(i)/Num_balloon) + W_balloon + W_cables + W_antenna + W_battery; %Total weight of one balloon [N]
-    %L(i) = Vol(i)*g*(rho - rho_i) - W(i); %- D; 
-    %a(i) = (L(i)*g)/W(i);
     
     i = i+1;
-    
-    
-
 end
 nexttile
 plot(2000:500:20000,Vol_balloon)
 nexttile
 plot(2000:500:20000,Area_solp)
+hold on
+plot(2000:500:20000,A_t)
+hold on
+
+
 
 %plot3(h,V,F_b)
 %contour3(hc,Vc,F_bc,25)
--- Radiation variance per altitude.py
+++ Radiation variance per altitude.py
@@ -20,7 +20,7 @@ tot_outp_0 = Outp_0/eff_0 # [W]
 temp_coef = -0.004 # [frac/deg C] 
 Req_eng = 70000 #[MWh] TU Delft yearly energy requirement
 
-alt = range(0,32500,500)
+alt = range(2000,32500,500)
 rho_lst = [];
 for i in alt:
     #[T, rho, p] = airdensity_calculator(alt);
@@ -46,15 +46,15 @@ for i in alt:
         
         
         
-    elif 20000 <= i <=32000:
-        p_0 = 5474.9
-        h_0 = 20000.
-        T_0 = 216.8
-        R = 287.
-        a_lapse = -0.001
-        T = T_0 + a_lapse * (i-h_0) #Temperature for gradient layer
-        p = p_0 * (T/T_0)**(-g/(a_lapse*R_air))
-        rho = p/(R_air*T)
+    #elif 20000 <= i <=32000:
+    #    p_0 = 5474.9
+    #    h_0 = 20000.
+     #   T_0 = 216.8
+      #  R = 287.
+       # a_lapse = -0.001
+        #T = T_0 + a_lapse * (i-h_0) #Temperature for gradient layer
+         #p = p_0 * (T/T_0)**(-g/(a_lapse*R_air))
+          #rho = p/(R_air*T)
     
     rho_lst.append(rho)
     
@@ -64,23 +64,21 @@ perc = []
 for i in range(len(rho_lst)): 
     perc.append(((rho_lst[i-1] - rho_lst[i]) / (rho_lst[0] - rho_lst[len(rho_lst)-1])) * 100)
 perc = perc[1:]
+print(perc)
+o_lst = []
 
-
-h = int(input("What's your altitude in m per 0/500/1000/1500/etc.: "))
-
-if h>0:
+#h = int(input("What's your altitude in m per 0/500/1000/1500/etc.: "))
+for h in range(2000,20500,500):
     d = perc[alt.index(h)-1]
-    print(d)
-    print(perc.index(d))
-    x = sum(perc[:(perc.index(d))+1])
-    print(x,"= total % decrease for total densities ratio")
+    x = sum(perc[:(perc.index(d))+1]) 
     o = 0.23*x
-    print(o,"% increase of solar radiation when given that the atmosphere only absorbs 23% of the solar radiation")
-    
-    
-    
-    
-if h==0:
-    print("0% increase")
+
+    o_lst.append(o)
+print(o_lst[1:])
+#i_lst = []
+#for i in o_lst[1:]:
+#    i_lst.append(i)
+#print(i_lst)
+
 
 
