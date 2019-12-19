e2eda44d9501e236d07d94d09a91aa3887d707d5
Pranav <pranavisabeast@gmail.com>
2019-12-19 12:02:05 +0100

    Experiment.py
    This is just a test for Erik Jan

---
+++ Python/experiment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 11:46:41 2019
+
+@author: pranav
+"""
+
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+start = time.time()
+
+
+
+
+end = time.time()
+print("Elapsed time: ", end-start)
