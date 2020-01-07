ce7e2c729457f42798fa701613938c0de2bf8cf8
MartvandeVen98 <martvandeven98@hotmail.com>
2020-01-07 12:35:05 +0100

    Create wing_box_stress_calculations.py

---
+++ Python/wing/wing_box_stress_calculations.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 12:06:16 2020
+
+@author: Mart
+
+This script is built to use the loads on the wing to calculate the bending stress in the wing box
+"""
+
+def bending_stress_wing_box(bending_moment_x, bending_moment_y, moment_of_inertia_x, moment_of_inertia_y, x_coordinate_from_neutral_axis, y_coordinate_from_neutral_axis):
+    bending_stress = (moment_of_inertia_y*bending_moment_x*y_coordinate_from_neutral_axis+bending_moment_y*moment_of_inertia_x*x_coordinate_from_neutral_axis)/moment_of_inertia_x*moment_of_inertia_y
+    return bending_stress
+
