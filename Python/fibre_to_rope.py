7ba5323072587d41f416a0a88d0f5ab3d47ae68f
tpsarblaster <t.p.s.arblaster@student.tudelft.nl>
2020-01-13 00:47:54 +0100

    rope updates
    now featuring a new kind of rope!!

--- Python/fibre_to_rope.py
+++ Python/fibre_to_rope.py
@@ -7,15 +7,22 @@ Created on Fri Jan 10 13:57:02 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-import statistics as stat
 from scipy.optimize import curve_fit
 
-diameter_of_ropes = [2.5,3,4,5,6,7,8,9,10,11,12,13,15,17] #[mm]
+#diameter_of_ropes = [2.5,3,4,5,6,7,8,9,10,11,12,13,15,17] #[mm]
 
-strength_of_ropes = [9.9,14.8,24.3,31.4,44.8,73.7,92.1,106,124,148,177,207,259,323] #[kN]
-8
-weight_of_ropes = [4.5,6.8,11.1,15.6,22.3,35.6,44.5,54,63,75.5,90,107,134,184] #[g/m]
-weight_of_ropes = np.asarray(weight_of_ropes)*0.001 #[kg/m]
+#strength_of_ropes = [9.9,14.8,24.3,31.4,44.8,73.7,92.1,106,124,148,177,207,259,323] #[kN]
+
+#weight_of_ropes = [4.5,6.8,11.1,15.6,22.3,35.6,44.5,54,63,75.5,90,107,134,184] #[g/m]
+#weight_of_ropes = np.asarray(weight_of_ropes)*0.001 #[kg/m]
+
+diameter_of_ropes = [5,6,7,8,9,10,11,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,48,52,56,60,64,72,80,88,96,104,112,120] #[mm]
+
+strength_of_ropes = [3.3,4,6.1,8.1,9,10.9,13.9,17.8,22,26.1,36,41,50.5,55,65,70,78,84.5,94,110,133,145,155,170,185,220,250,280,325,400,450,525,625,690,790,900] #[1000kg]
+strength_of_ropes = np.asarray(strength_of_ropes)*9.81 #[kN]
+
+weight_of_ropes = [1.4,2.1,2.7,3.5,4.2,4.6,6.2,9.0,12.0,14.0,19.0,22,25.5,30,32.5,39,43,46,53,60,68,73,83,100,125,143,175,200,230,300,350,430,500,600,700,800] #[kg/100m]
+weight_of_ropes = np.asarray(weight_of_ropes)*0.01 #[kg/m]
 
 area_of_ropes = [] #[m^2]
 
@@ -58,14 +65,14 @@ plt.plot(x_data_rope,y_data_rope_1,'ko',label='Dyneema D12 MAX rope')
 plt.plot(x_value_range,f(np.asarray(x_value_range),popt1[0]),'k-.',label='Rope approximation')
 plt.legend()
 
-x_value_range = x_value_range*3
+x_value_range = x_value_range*300
 
-b_for_weight_per_area = (strength_of_fibre*10**6/density_of_fibre-popt1[0])*1000/(strength_of_fibre*10**9)
+b_for_weight_per_area = (strength_of_fibre*10**9)/((strength_of_fibre*10**6/density_of_fibre-popt1[0])*1000)
 
 plt.subplot(1, 2, 2)
-plt.xlabel('weight increase (%)')
-plt.ylabel('area of pure fibre (10^-5 m^2)')
-plt.plot(x_value_range*100,f(np.asarray(x_value_range),b_for_weight_per_area*10**5),'k-') #,label='Rope performance'
+plt.xlabel('area of pure fibre (10^-5 m^2)')
+plt.ylabel('weight increase (%)')
+plt.plot(x_value_range,f(np.asarray(x_value_range),b_for_weight_per_area)/1000,'k-') #,label='Rope performance'
 #plt.legend()
 
 plt.show()
